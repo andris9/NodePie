aa05de73781d68a34fae7ff02ae22b416f5d5be6
Andris Reinman <andris@kreata.ee>
2014-05-12 16:21:03 +0300

    Reformatted code

--- test/test.js
+++ test/test.js
@@ -8,11 +8,11 @@ request(feed_url, function(error, xml){
     if(error){
         throw error;
     }
-    
+
     var np = new NodePie(xml), item;
-    
+
     np.init();
-    
+
     console.log("\n" + np.getTitle());
     console.log(new Array((np.getTitle() || "").length + 1).join("="));
     console.log(np.getDescription());
@@ -22,17 +22,17 @@ request(feed_url, function(error, xml){
     console.log("Source encoding:", np.getEncoding());
     console.log("Updated:", np.getDate());
     console.log("Image:", np.getImage());
-    
+
     for(var i=0, len = np.getItemQuantity(3); i<len; i++){
         item = np.getItem(i);
-        
+
         console.log("");
         console.log(item.getTitle());
         console.log(new Array((item.getTitle() || "").length + 1).join("-"));
         console.log(item.getPermalink());
         console.log("Created", item.getDate(), "\n");
         console.log(item.getDescription());
-        
+
     }
 
 });
@@ -44,26 +44,26 @@ function request(url, callback){
             port: 80,
             path: urlparts.pathname + (urlparts.search || "")
         };
-    
+
     http.get(options, function(res) {
         if(res.statusCode != 200){
             return callback(new Error("Invalid response code "+res.statusCode));
         }
-        
+
         var body = "";
         res.on("data", function(chunk){
             body += chunk.toString("binary");
         });
-        
+
         res.on("end", function(){
             callback(null, new Buffer(body, "binary"));
         });
-        
+
         res.on("error", function(e){
             callback(e);
         });
-        
+
     }).on('error', function(e) {
         callback(e);
-    });    
+    });
 }
